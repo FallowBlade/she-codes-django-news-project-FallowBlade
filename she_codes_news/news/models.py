aef7bad4bb96901288e8fde68c77d60317a3f2aa
FallowBlade <113985916+FallowBlade@users.noreply.github.com>
2022-12-10 12:00:54 +0800

    Feature: Image URL
    Created images URLField feature on Form by creating in models and linking to html StoryCard and Story.

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -3,13 +3,22 @@ from django.db import models
 
 
 class NewsStory(models.Model):
+
+    class Meta:
+        ordering = ['-pub_date']
+        
+
+
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE
     )
     pub_date = models.DateTimeField()
+    image_url =  models.URLField(default="https://images6.fanpop.com/image/photos/41200000/Cat-cats-41239805-1600-900.jpg")
+    # could also do in URLField(blank=True)
     content = models.TextField()
+
 
 # NOTE 
 # properties are title, author, pub_date
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -13,11 +13,17 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
+        # context['all_stories'] = NewsStory.objects.all()
+        context['old_stories'] = NewsStory.objects.all().order_by('pub_date')[:4]
+        # old_stories orders your news stories by date.
         return context
         # This is saying get all of our news stories and use it in the index
         # latest stories = Get all news stories, but ONLY the first 4 stories (0,1,2,3) poisition
         # all_stories = all news stories
+
+
+# how to move what you want on your news page.
+
 
 class AddStoryView(generic.CreateView):
     form_class = StoryForm
@@ -33,6 +39,11 @@ class StoryView(generic.DetailView):
 	model = NewsStory
 	template_name = 'news/story.html'
 	context_object_name = 'story'
+
+    # def img_form(request)
+    #     form.instance.image_url = self.request.form
+    #     return render( request, ).img_form(form)
+
 # Has a CLASS-BASED VIEW - part 4 of tutorial talk about this.
 # The one's we are doing in class are class-based views
 # CBW is similar to Functions but bit differemt
